0a04e36e6f2e3d193b25e82b30d6293b856acbe4
wuyinlei <wuyinlei19931118@hotmail.com>
2015-11-17 20:59:09 +0800

    完成简单的一个遥控器的界面，在这学到了样式的使用和include标签

---
+++ src/main/java/com/example/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,20 @@
+package com.example.remotecontrol;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Window;
+
+public class RemoteControlActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return new RemoteControlFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+    }
+}
